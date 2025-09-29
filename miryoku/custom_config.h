f1c3a1a5d3a53593ccaf603e8b5a917edc4f0063
mrt181 <mrt181@users.noreply.github.com>
2025-09-29 16:28:30 +0200

    Add Graphite layout with International layer for Chocofi
    - Replaces QWERTY base with Graphite layout
    - Adds INTL layer with German umlauts (ä ë ï ö ü ß)
    - Adds international characters (€ £ ¥ ñ ç ÿ)
    - Adds dead keys for accent combinations
    - Accessible via Q and / layer-tap keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,12 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Custom configuration for Graphite layout
+// Created for mrt181
+
+// Use Graphite layout as the base layer instead of QWERTY
+#define MIRYOKU_LAYER_BASE \
+&kp B,             &kp L,             &kp D,             &kp W,             &kp Z,                &kp SEMI,          &kp F,             &kp O,             &kp U,             &kp J, \
+U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTRL, T),    U_MT(LSHFT, S),    &kp G,                &kp Y,             U_MT(LSHFT, H),    U_MT(LCTRL, A),    U_MT(LALT, E),     U_MT(LGUI, I), \
+U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,                &kp K,             &kp P,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH), \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),   U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
